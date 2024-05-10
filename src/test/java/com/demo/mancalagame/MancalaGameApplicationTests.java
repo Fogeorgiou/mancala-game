ab6e38f5dd84a418409bfb75f7d7884a9b788052
fotin <Fotinak1>
2024-05-10 21:55:51 +0200

    Initial application setup

---
+++ src/test/java/com/demo/mancalagame/MancalaGameApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.demo.mancalagame;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MancalaGameApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
